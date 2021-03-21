19d07b818234f55c67c764f20dd1eb2dccca128b
agastya-mishra1 <78002464+agastya-mishra1@users.noreply.github.com>
2021-03-20 20:50:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function change1(){
+window.alert("This is the Theatre where we perform shows and other performances. It is also the place where we perform our Talent Shows. The Theatre is considered as one of the most entertaining places in whole school. ");   
+}
+function change2(){
+window.alert("This is the Library where we read/ collect books for the week. It is a very cozy place where you can read with absolute calmness and joyment. The Library is considered as the most quiet and calm room in the school.");   
+}
+function change3(){
+window.alert("This is a classroom. Most of the classes in Aylesbury Public School look like this. These are the rooms where all the students in the whole wide school study and learn. A classroom approximately has 20 - 30 students. ");   
+}
